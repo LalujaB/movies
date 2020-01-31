9e78d33286cdad46ea74edfe73458812869ee94e
Ime Prezime <moj_email@gmail.com>
2020-01-31 22:00:49 +0100

    Zadatak 4

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,9 @@
+
+    <div class="navbar">
+        <div class="navbar-inner">
+            <ul class="nav">
+                <li><a href="/movies">Movies</a></li>
+                <li><a href="/add">Add</a></li>
+            </ul>
+        </div>
+    </div>
--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -1,17 +1,18 @@
-<!doctype html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Movies</title>
-    </head>
-    <body>
+@extends('layouts/master')
+
+@section('title')
+    Movies
+@endsection
+
+
+    @include('layouts/navbar')
+
+@section('content')
+
         @foreach($movies as $movie)
             <div>
                 <h2><a href="movies/{{$movie->id}}">{{$movie->title}}</a></h2> <br> {{$movie->storyline}}
             </div>
             @endforeach
-    </body>
-    </html>
+
+@endsection
